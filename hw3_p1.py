2cd362db72fb20dec34fdd2335637daee926fca9
jeremydoan <jdoan31@gmail.com>
2016-04-02 18:20:10 -0700

    Add hw3 problem 1 program

---
+++ hw3_p1.py
@@ -0,0 +1,22 @@
+from math import *
+
+def g_x(x):
+    return (exp((-x**2)/2))
+
+def gx_prime(x):
+    return ((exp((-x**2)/2))*(-x))
+
+def central_diff(x, h):
+    return ((g_x(x+h) - g_x(x-h))/(2*h)) 
+
+def problem_3():
+    true = gx_prime(1.4)
+    print("g'(1.4) = %1.20f" %(true))
+    print("            h                          g(1.4)                        r.error         ")
+    print("  ------------------------     -------------------------     ------------------------")
+    for i in range(1,21):
+        value = central_diff(1.4,(10.**(-i)))
+        print("   %.20f       %2.20f       %.20f    " %((10.**(-i)), value, abs((true-value))/abs(true)))
+    return
+
+problem_3()
