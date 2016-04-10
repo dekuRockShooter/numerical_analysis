1eb92847cbb976cfb794b49eb174706bae14ab49
sfsugator <amuller@mail.sfsu.edu>
2016-04-10 12:05:53 -0700

    added comments

--- HW3/cdf.py
+++ HW3/cdf.py
@@ -1,5 +1,7 @@
 fx = [1.00000000, 0.90483742, 0.81873075, 0.74081822, 0.67032005, 0.60653066, 0.54881164, 0.49658530, 0.44932896]
-
+"""Centered Difference using Richardson's Extrapolation Method to find the derivative of a function at a point
+    FofX: a list of y-values
+"""
 def cdfRichardson(FofX):
     fPrime =[]
     fPrime.append((FofX[8] - FofX[0])/(2*0.4))
@@ -10,7 +12,7 @@ def cdfRichardson(FofX):
     fPrime.append(fPrime[4] + (fPrime[4] - fPrime[2])/15)
 
     return fPrime
-
+"""A method to show the contents of the list"""
 def show(array):
     for i in array:
         print i
