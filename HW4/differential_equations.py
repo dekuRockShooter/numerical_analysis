06536275f57fba52063a3b1947fb050317252eb7
dekuRockShooter <dekuRockShooter@gmail.com>
2016-04-14 12:40:33 -0700

    Add code to do problems 1 and 2.

--- HW4/differential_equations.py
+++ HW4/differential_equations.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 
 def euler_generator(f, t0, x0, h):
     """Approximate a first order ODE using Euler's Method.
@@ -19,6 +20,7 @@ def euler_generator(f, t0, x0, h):
     """
     x = x0
     t = t0
+    yield x
     while True:
         x = x + (h*f(t, x))
         t = t + h
@@ -43,6 +45,7 @@ def mod_euler_generator(f, t0, x0, h):
     """
     x = x0
     t = t0
+    yield x
     while True:
         m1 = f(t, x)
         t = t + h
@@ -69,6 +72,7 @@ def rk4_generator(f, t0, x0, h):
     """
     t = t0
     x = x0
+    yield x
     while True:
         k1 = h*f(t, x)
         k2 = h*f(t + (0.5*h), x + (0.5*k1))
@@ -143,10 +147,11 @@ def rk4_n2_generator(f1, f2, t0, x0, y0, h):
         m3 = h*f2(t + (0.5*h), x + (0.5*k2), y + (0.5*m2))
         k4 = h*f1(t + h, x + k3, y + m3)
         m4 = h*f2(t + h, x + k3, y + m3)
-        x = x + (1.0/6)*(k1 + (2*k2) + (2*k3) + k4)
-        y = y + (1.0/6)*(m1 + (2*m2) + (2*m3) + m4)
+        x = x + (1.0/6)*(k1 + (2.0*k2) + (2.0*k3) + k4)
+        y = y + (1.0/6)*(m1 + (2.0*m2) + (2.0*m3) + m4)
         t = t + h
         yield (x, y)
+
 
 # Test both Eulers and RK4 using examples 6.4, 6.5, and 6.6 from the
 # lecture notes.
@@ -169,6 +174,7 @@ h = 0.1
 #f2 = lambda t,x,y: -2.4*x + 1.6*y + 3.6
 #p2 = iter(euler_n2_generator(f1, f2, 0.0, 0.0, 0.0, h))
 #p2_1 = iter(rk4_n2_generator(f1, f2, 0.0, 0.0, 0.0, h))
+
 # From table 5.20 in Burden and Faires ed. 9, chapter 5.9.
 f1 = lambda t,x,y: y
 f2 = lambda t,x,y: (math.e**(2 * t) * math.sin(t)) - (2*x) + (2*y)
@@ -181,3 +187,86 @@ p2_1 = iter(rk4_n2_generator(f1, f2, 0.0, -0.4, -0.6, h))
 print
 for k in range(6):
     print next(p2), next(p2_1)
+
+f1 = lambda t,x,y: y
+f2 = lambda t,x,y: -4*(math.pi**2)*x
+p2 = iter(euler_n2_generator(f1, f2, 0.0, 0.0, -1.0, h))
+p2_1 = iter(rk4_n2_generator(f1, f2, 0.0, 0.0, -1.0, h))
+print
+for k in range(6):
+    print '****'
+    print next(p2)
+    print next(p2_1)
+
+
+def plot(x_vals, y_vals, x2_vals, y2_vals, x_min, x_max, y_min, y_max, title):
+    plt.axvline(x=0,color='black')
+    plt.axhline(y=0,color='black')
+    plt.axis([x_min, x_max, y_min, y_max], 1/6)
+    plt.plot(x_vals, y_vals, color='red', label='Actual values')
+    plt.plot(x2_vals, y2_vals, color='blue', label='Actual values')
+    plt.title(title)
+    plt.show()
+
+def problem_1():
+    f = lambda t,x: t - (2*x)
+    t_vals_exact = [k/100.0 for k in range(201)]
+    x_vals_exact = [1.25*math.e**(-2*t) + 0.25*(2*t - 1) for t in t_vals_exact]
+    #f = lambda t,x: t - x
+    #x_vals_exact = [t - 1 + 1.5*(math.e**(-t)) for t in t_vals_exact]
+    t0 = 0.0
+    x0 = 1.0
+    for h in (0.2, 0.1, 0.05):
+        title1 = 'Euler with step size ' + str(h)
+        title2 = 'Modified Euler with step size ' + str(h)
+        title3 = 'RK4 with step size ' + str(h)
+        titles = (title1, title2, title3)
+        euler = iter(euler_generator(f, t0, x0, h))
+        mod_euler = iter(mod_euler_generator(f, t0, x0, h))
+        rk4 = iter(rk4_generator(f, t0, x0, h))
+        t_vals = [k*h for k in range(int(math.ceil(3.0/h)))]
+        x_vals_exact_2 = [1.25*math.e**(-2*t) + 0.25*(2*t - 1) for t in t_vals]
+        x_vals = []
+        abs_err = []
+        iterators = [euler, mod_euler, rk4]
+        for sequence, title in zip(iterators, titles):
+            for x_exact in x_vals_exact_2:
+                x_k = next(sequence)
+                x_vals.append(x_k)
+                abs_err.append(abs(x_exact - x_k))
+            plot(t_vals, x_vals, t_vals_exact, x_vals_exact,
+                 0.0, 2.0, 0.0, 1.5, title)
+            plot(t_vals, abs_err, [0], [0],
+                 0.0, 2.0, 0.0, .12, title + ' absolute error')
+            x_vals[:] = []
+            abs_err[:] = []
+
+def problem_2():
+    f1 = lambda t,x,y: y
+    f2 = lambda t,x,y: -4 * (math.pi**2) * x
+    t_vals_exact = [k/100.0 for k in range(121)]
+    x_vals_exact = [-math.cos(2*math.pi*t) for t in t_vals_exact]
+    for h in (0.1, 0.05, 0.01, -1):
+        title = ''
+        if h < 0:
+            h = 0.1
+            p2 = iter(rk4_n2_generator(f1, f2, 0.0, 0.0, -1.0, h))
+            title = 'RK4 with step size 0.1 absolute error'
+        else:
+            p2 = iter(euler_n2_generator(f1, f2, 0.0, 0.0, -1.0, h))
+            title = 'Euler with step size ' + str(h) + ' absolute error'
+        t_vals = [k*h for k in range(int(math.ceil(1.8/h)))]
+        x_vals_exact_2 = [-math.cos(2*math.pi*t) for t in t_vals]
+        x_vals = []
+        abs_err = []
+        for x_exact in x_vals_exact_2:
+            x_k = list(next(p2))[1]
+            x_vals.append(x_k)
+            abs_err.append(abs(x_exact - x_k))
+        plot(t_vals, x_vals, t_vals_exact, x_vals_exact,
+             0.0, 1.2, -1.1, 1.1, title)
+        plot(t_vals, abs_err, [0], [0],
+             0.0, 1.2, 0.0, 1e-2, title)
+
+#problem_1() # TODO: table summary
+problem_2() # TODO: table summary
